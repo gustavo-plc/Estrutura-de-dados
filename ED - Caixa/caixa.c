3dbfeff8bcb3a4beba39c481fc3e55f1a4ff0985
Gustavo Cunha <gustavoparreira@hotmail.com>
2024-02-21 17:13:45 -0300

    implementação da estrutura e das funções

--- ED - Caixa/caixa.c
+++ ED - Caixa/caixa.c
@@ -25,24 +25,31 @@ Caixa *criar_caixa(float comp, float largura, float altura)
 
 void liberar_caixa(Caixa *c)
 {
+    if(!c) // tratamento de exceção, para caso se queira liberar uma caixa que não existe.
+        printf("A caixa indicada para liberação não existe!\n");
     free(c);
     printf("A caixa foi liberada da memória");
 }
 
 void exibir_caixa(Caixa *c)
 {
-
-
+    if(!c) // tratamento de exceção, para caso se queira exibir uma caixa que não existe.
+        printf("A caixa indicada para exibição não existe!\n");
+    printf("A largura, comprimento e altura da caixa é respectivamente: %.2f %.2f %.2f\n", c->largura, c->comprimento, c->altura);
 }
 
 float area_caixa(Caixa *c)
 {
-
-
+    if(!c) // tratamento de exceção, para caso se queira calcular a área de uma caixa que não existe.
+        printf("A caixa indicada para cálculo de área não existe!\n");
+    float area = (c->comprimento) * (c->altura) * 2 + (c->comprimento) * (c->largura) * 2 + (c->altura) * (c->largura) * 2;
+    return printf("A área total da caixa é: %.2f\n", area);
 }
 
 float volume_caixa(Caixa *c)
 {
-
-
+    if(!c) // tratamento de exceção, para caso se queira calcular o volume de uma caixa que não existe.
+        printf("A caixa indicada para cálculo de volume não existe!\n");
+    float volume = (c->comprimento) * (c->altura) * (c->largura);
+    return printf("O volume da caixa é: %.2f\n", volume);
 }
