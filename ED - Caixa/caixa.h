141b12d34207f8a4f8fd6636d2c93df5d205fee2
Gustavo Cunha <gustavoparreira@hotmail.com>
2024-02-21 16:47:21 -0300

    finalização da caixa.h

--- ED - Caixa/caixa.h
+++ ED - Caixa/caixa.h
@@ -1,15 +1,13 @@
-
+//arquivo header, somente para cabeçalho das estruturas e operações. Sem detalhar a estrurura e nem as operações.
 #ifndef CAIXA_H
 #define CAIXA_H
 
+typedef struct caixa Caixa;
 
-
-
-
-
-
-
-
-
+Caixa *criar_caixa(float comp, float largura, float altura);
+void liberar_caixa(Caixa *c);
+void exibir_caixa(Caixa *c);
+float area_caixa(Caixa *c);
+float volume_caixa(Caixa *c);
 
 #endif
