9051c1e9862f5fc89ba193680393d7a1120d4dfe
Gustavo Cunha <gustavoparreira@hotmail.com>
2024-03-01 10:27:19 -0300

    início da implementação da busca linear

---
+++ ED - Busca linear/busca_l.h
@@ -0,0 +1,11 @@
+#ifndef BUSCA_L
+#define BUSCA_L
+
+typedef struct busca_l Busca_l;
+
+void cria_vetor(int qtd_elementos, int valores);
+void busca_linear(int tamanho, int vetor[], int valor);
+
+
+
+#endif
