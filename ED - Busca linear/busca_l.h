3237bc7ea7f47d8a21963ead0bf5a83332f0068c
Gustavo Cunha <gustavoparreira@hotmail.com>
2024-03-01 10:31:28 -0300

    estrutura vetor

--- ED - Busca linear/busca_l.h
+++ ED - Busca linear/busca_l.h
@@ -1,12 +1,11 @@
 #ifndef BUSCA_L
 #define BUSCA_L
 
-typedef struct busca_l Busca_l;
+typedef struct vetor Vetor;
 
 void cria_vetor(int qtd_elementos, int valores);
 void exibe_vetor(int vetor[]);
 int busca_linear(int tamanho, int vetor[], int valor);
-
 
 
 #endif
