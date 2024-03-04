d29d584315281c204e202916a47082ea47bdcf16
Gustavo Cunha <gustavoparreira@hotmail.com>
2024-03-04 10:15:28 -0300

    ainda erro na execução

--- ED - Ordenação/ordena.c
+++ ED - Ordenação/ordena.c
@@ -3,6 +3,7 @@
 
 void bubbleSort(int arr[], int n)
 {
+    int i, j;
     for (int i = 0; i < n - 1; i++)
     {
         for (int j = 0; j < n - i - 1; j++) ;  //para cada iteração do laço externo, o laço interno percorre o array
@@ -19,6 +20,7 @@ void bubbleSort(int arr[], int n)
 
 void bubbleSort_invertido(int arr[], int n) //função para ordem decrescente
 {
+    int i, j;
     for (int i = 0; i < n - 1; i++) // no invertido, continuo tendo que fazer n-1 iterações
     {
         for (int j = n - i - 1; j > 0; j--)
