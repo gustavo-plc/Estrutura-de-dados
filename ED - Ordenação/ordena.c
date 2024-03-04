1ea7c89deab10ad3a706fb743bee99e0c6e39980
Gustavo Cunha <gustavoparreira@hotmail.com>
2024-03-04 10:37:46 -0300

    selection sort implementado

--- ED - Ordenação/ordena.c
+++ ED - Ordenação/ordena.c
@@ -4,9 +4,9 @@
 void bubbleSort(int arr[], int n)
 {
     int i, j;
-    for (int i = 0; i < n - 1; i++)
+    for (i = 0; i < n - 1; i++)
     {
-        for (int j = 0; j < n - i - 1; j++) ;  //para cada iteração do laço externo, o laço interno percorre o array
+        for (j = 0; j < n - i - 1; j++) ;  //para cada iteração do laço externo, o laço interno percorre o array
         {
             if (arr[j]>arr[j+1])
             {
@@ -21,9 +21,9 @@ void bubbleSort(int arr[], int n)
 void bubbleSort_invertido(int arr[], int n) //função para ordem decrescente
 {
     int i, j;
-    for (int i = 0; i < n - 1; i++) // no invertido, continuo tendo que fazer n-1 iterações
+    for (i = 0; i < n - 1; i++) // no invertido, continuo tendo que fazer n-1 iterações
     {
-        for (int j = n - i - 1; j > 0; j--)
+        for (j = n - i - 1; j > 0; j--)
         {
             if (arr[j]>arr[j-1])
             {
@@ -33,4 +33,29 @@ void bubbleSort_invertido(int arr[], int n) //função para ordem decrescente
             }
         }
     }
+}
+
+void selectionSort(int arr[], int n)
+{
+    int i, j, min, temp;
+    for (i = 0; i < n - 1; i++)
+    {
+        min = i;
+        for (j = i + 1; j < n ; j++)
+        {
+            if(arr[j] < arr[min])
+                min = j;
+        }
+        if (min != i)
+        {
+            temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+}
+
+void selectionSortV2(int arr[], int n)
+{
+    
 }
--- ED - Ordenação/ordena.h
+++ ED - Ordenação/ordena.h
@@ -3,5 +3,7 @@
 
 void bubbleSort(int arr[], int n);
 void bubbleSort_invertido(int arr[], int n);
+void selectionSort(int arr[], int n);
+void selectionSortV2(int arr[], int n);
 
 #endif
