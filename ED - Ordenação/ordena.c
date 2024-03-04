f4454124c92ab50fb8a000efae50d592a91ec10f
Gustavo Cunha <gustavoparreira@hotmail.com>
2024-03-04 09:02:35 -0300

    escrita dos laços do bubble sort, ainda sem if

--- ED - Ordenação/ordena.c
+++ ED - Ordenação/ordena.c
@@ -3,10 +3,17 @@
 
 void bubbleSort(int arr[], int n)
 {
-
+    int i, j, temp;
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            
+        }
+    }
 }
 
 void bubbleSort_invertido(int arr[], int n)
 {
-    
+
 }
