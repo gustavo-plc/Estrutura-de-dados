b51b09c58fdd77eec8b416bf7e4b083e24a5c734
Gustavo Cunha <gustavoparreira@hotmail.com>
2024-03-04 09:08:30 -0300

    implementação do bubble sort normal

--- ED - Ordenação/ordena.c
+++ ED - Ordenação/ordena.c
@@ -6,9 +6,14 @@ void bubbleSort(int arr[], int n)
     int i, j, temp;
     for (int i = 0; i < n - 1; i++)
     {
-        for (int j = 0; j < n - i - 1; j++)
+        for (int j = 0; j < n - i - 1; j++) ;  //para cada iteração do laço externo, o laço interno percorre o array
         {
-            
+            if (arr[j]>arr[j+1])
+            {
+                temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+            }
         }
     }
 }
