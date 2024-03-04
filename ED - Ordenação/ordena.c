7841f09250e761f7d297243bbcf341d47ce34928
Gustavo Cunha <gustavoparreira@hotmail.com>
2024-03-04 10:11:45 -0300

    retirada das declarações externas e mantidas somente no laço

--- ED - Ordenação/ordena.c
+++ ED - Ordenação/ordena.c
@@ -3,14 +3,13 @@
 
 void bubbleSort(int arr[], int n)
 {
-    int i, j, temp;
     for (int i = 0; i < n - 1; i++)
     {
         for (int j = 0; j < n - i - 1; j++) ;  //para cada iteração do laço externo, o laço interno percorre o array
         {
             if (arr[j]>arr[j+1])
             {
-                temp = arr[j];
+                int temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
             }
@@ -20,16 +19,15 @@ void bubbleSort(int arr[], int n)
 
 void bubbleSort_invertido(int arr[], int n) //função para ordem decrescente
 {
-    int i, j, temp;
     for (int i = 0; i < n - 1; i++) // no invertido, continuo tendo que fazer n-1 iterações
     {
         for (int j = n - i - 1; j > 0; j--)
         {
-            if (arr[j+1]>arr[j])
+            if (arr[j]>arr[j-1])
             {
-                temp = arr[j + 1];
-                arr[j + 1] = arr[j];
-                arr[j] = temp;
+                int temp = arr[j];
+                arr[j] = arr[j-1];
+                arr[j-1] = temp;
             }
         }
     }
