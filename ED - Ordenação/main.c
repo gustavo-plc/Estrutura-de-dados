fac6ff1643fb46557a205d1f658d19e19f26f293
Gustavo Cunha <gustavoparreira@hotmail.com>
2024-03-10 18:40:35 -0300

    atualização do header com novas funções

--- ED - Ordenação/main.c
+++ ED - Ordenação/main.c
@@ -8,22 +8,23 @@ int main()
 
     int array2[] = {15, 13, 17, 11, 12, 14, 19, 18};
 
-    //selectionSortV2(array, qtd);
-    //bubbleSort_invertido(array2, qtd);
+    int array3[] = {25, 23, 27, 21, 22, 24, 29, 28};
+
 
     bubbleSort_invertido(array, qtd);
 
     selectionSort(array2, qtd);
 
+    heapSort(qtd, array3)
+
     for (int i = 0; i < qtd; i++)
-    {
         printf("O indice %d do array ordenado eh: %d\n", i, array[i]);
-    }
     
-        for (int i = 0; i < qtd; i++)
-    {
+    for (int i = 0; i < qtd; i++)
         printf("O indice %d do array ordenado eh: %d\n", i, array2[i]);
-    }
+
+    for (int i = 0; i < qtd; i++)
+        printf("O indice %d do array ordenado eh: %d\n", i, array3[i]);
 
     return 0;
 }
--- ED - Ordenação/ordena.h
+++ ED - Ordenação/ordena.h
@@ -5,5 +5,9 @@ void bubbleSort(int arr[], int n);
 void bubbleSort_invertido(int arr[], int n);
 void selectionSort(int arr[], int n);
 void selectionSortV2(int arr[], int n);
+void heapSort(int n, int arr[n]);
+void construir_heap(int n, int arr[n]);
+void ajusta_heap(int n, int arr[n], int i);
+
 
 #endif
