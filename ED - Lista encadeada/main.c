1cd03597b61361cce3e2e433281e68ea9e3555c7
Gustavo Cunha <gustavoparreira@hotmail.com>
2024-03-20 14:25:32 -0300

    criando e encadeando nós

--- ED - Lista encadeada/main.c
+++ ED - Lista encadeada/main.c
@@ -1,16 +1,23 @@
 int main()
 {
+    // -- CRIANDO NÓS E JÁ ENCADEANDO --
+
     //primeira etapa: é criado um primeiro nó (com dado e endereço do próximo, que no caso é NULL)
     No *inicio = criarNo(1);
 
     //segunda etapa: é criado um segundo nó, com dado e o endereço do próximo, que no caso é NULL),
-    //mas o endereço desse nó recém criado (retorno da função criarNO) vai ser armazenado na variável 
+    //mas o endereço desse nó recém criado (retorno da função criarNO) vai ser armazenado 
     //na parte de endereço do nó inicio. Ou seja, o nó inicio irá apontar para o segundo nó criado. 
-    inicio->prox = criarNo(2); //ao atributo próx do ponteiro início, é atribuído o retorno da função criarNo, que retorna o endereço do nó criado pelo malloc
 
-    //terceira etapa
-    inicio->prox->prox = criarNo(3);
+    inicio->prox = criarNo(2); //ao atributo próx da estrutura do ponteiro início (ponteiro para Nó), é atribuído o retorno da função criarNo, 
+    //que retorna o endereço do nó criado pelo malloc
 
+    //terceira etapa: será criado um novo nó por meio da função criarNo e seu retorno (endereço do nó criado) será armazenado no atributo prox do atributo próx do nó início.
+    inicio->prox->prox = criarNo(3); //o início, como uma variável do tipo nó, possui duas partes, uma de dados e outra que é um ponteiro do tipo Nó (estrutura de Nó) que 
+    //por sua vez tem também duas partes: o campo de dados e um ponteiro do tipo Nó, que por sua vez também tem dois campos: dados e ponteiro
+    //Dessa forma, é possível acessar o atributo prox, do atributo próx do nó início.
+
+    
     inicio->prox->prox->prox = criarNo(4);
 
     /*inicio = criar_no (1);
