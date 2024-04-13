c549f3bc28238aa4d3f7256bb21624020d67bac1
Gustavo Cunha <gustavoparreira@hotmail.com>
2024-04-13 09:57:46 -0300

    criação da função de busca binária

--- ED - Busca binária/busca_b.c
+++ ED - Busca binária/busca_b.c
@@ -26,6 +26,38 @@ Vetor* cria_vetor(int qtd_elementos)
 
 int buscaBinaria(Vetor *vetor, int tamanho, int valor)
 {
-   
+    if(!vetor)
+    {
+        printf("\nProblema ao carregar o vetor!");
+        return -1;
+    }
+    if(tamanho == 0)
+    {
+        printf("\nVetor vazio!");
+        return -1;
+    }
+    int inicio = 0;
+    int fim = tamanho - 1;
+    int meio = (inicio + fim) / 2;
+    while(inicio != fim)
+    {
+        if(valor == vetor->vetor[meio])
+        {
+            printf("Valor encontrado na posicao: %d", meio);
+            return -1;
+        }
+        else
+        {
+            if(valor > vetor->vetor[meio])
+            {
+                inicio = meio + 1;
+            }
+            else
+            {
+                fim = meio - 1;
+                meio = (inicio + fim) / 2;
+            }
+        }
+    }
 }
 
