6a607b7e06402323f5842801d69015fb8c20361c
Gustavo Cunha <gustavoparreira@hotmail.com>
2024-05-24 15:02:42 -0300

    atualização do tad avl

--- ED - TAD Avl/avltree.c
+++
@@ -1,5 +0,0 @@
-int balancingFactor(AVLTree *avltree)
-{
-
-    
-}
--- ED - TAD Avl/main.c
+++ ED - TAD Avl/main.c
@@ -1,19 +1,50 @@
 #include <stdio.h>
-#include <avltree.h>
+#include <stdlib.h>
 
+#include "../includes/TADAVL.h"
 
+struct avltree {
+  AVLTree *esq;
+  AVLTree *dir;
+  int dado;
+  void (*show)(AVLTree *avltree);
+};
 
-int main()
-{
+void exibir(AVLTree *avltree) {
+  if(avltree) printf("(%d)", avltree->dado);
+}
 
+AVLTree *novaArvore(int dado) {
+  AVLTree *raiz = malloc(sizeof(AVLTree));
+  if(raiz) {
+    raiz->dir = NULL;
+    raiz->esq = NULL;
+    raiz->dado = dado;
+    raiz->show = exibir;
+  }
+  return raiz;
+}
 
+// a serem implementados
+int balancingFactor(AVLTree *avltree);
+unsigned int height(AVLTree *avltree);
+AVLTree *rotateLeft(AVLTree *avltree);
+AVLTree *rotateRight(AVLTree *avltree);
+AVLTree *insertNode(AVLTree *avltree, int dado);
+AVLTree *deleteNode(AVLTree *avltree, int dado);
 
+// exibir
+void preOrder(AVLTree *avltree);
+void inOrder(AVLTree *avltree);
+void posOrder(AVLTree *avltree);
 
-
-
-
-
-
-
-    
+int main() {
+  AVLTree *raiz = novaArvore(5);
+  raiz->esq = novaArvore(3);
+  raiz->dir = novaArvore(7);
+  raiz->show(raiz);
+  raiz->esq->show(raiz->esq);
+  raiz->dir->show(raiz->dir);
+  
+  return 0;
 }
