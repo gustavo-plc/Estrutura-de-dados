5bbaaf8ed8dd98cfa079cda6710744c485e6a374
Gustavo Cunha <gustavoparreira@hotmail.com>
2024-05-27 09:35:28 -0300

    atualizaçãodo código

--- ED - Tabelas Hash/main.c
+++ ED - Tabelas Hash/main.c
@@ -2,57 +2,62 @@
 #include <stdlib.h>
 
 typedef struct entry Entry;
+typedef struct list List;
 typedef struct hash_map HashMap;
 
-struct entry //par chave-valor, representado como um nó, que tem ponteiro que aponta para o próx
-    {
-        int key;
-        int value;
-        Entry *prox;
-    };
+struct entry {
+  int key;
+  int value;
+  Entry *prox;
+};
 
-struct hash_map
-    {
-        Entry **entries;
-        int length;
-        // void (*hashFunction)(int key) // ponteiro para funções
-    };
+struct list {
+  Entry *start;
+  int length;
+};
 
-int hash1x1(int x)
-    {
-        return x;
+struct hash_map {
+  List *lists;
+  int length;
+  // void (*hashFunction)(int key); // ponteiro para função
+};
+
+int hash1x1(int x) {
+  return x;
+}
+
+int hash1xN(int x) {
+  return x % 10;
+}
+
+Entry *newEntry(int key, int value) {
+  Entry *entry = malloc(sizeof(Entry));
+  if(entry) {
+    entry->key = key;
+    entry->value = value;
+  }
+  return entry;
+}
+
+HashMap *newHashMap(int tableSize) {
+  HashMap *map = malloc(sizeof(HashMap));
+  if(map) { // inicializar o map
+    map->length = 0;
+    map->lists = calloc(sizeof(List), tableSize);
+    if(!map->lists) {
+      free(map);
+      return NULL;
     }
-
-int hash1xn(int x)
-    {
-        return x % 10;
-    }
-
-HashMap *newHashMap(int tableSize) // representa a criação do modelo de lista encadeada
-    {
-        HashMap *map = malloc(sizeof(HashMap));
-        if(map)
-            {
-                map->entries = calloc(sizeof(Entry*), tableSize);
-                map->length = 0;
-            }
-        return map;
-    }
-
-Entry *newEntry(int key, int value)
-    {
-        Entry *entry = malloc(sizeof(Entry));
-        if(entry)
-            {
-                entry->key = key;
-                entry->value = value;
-            }
-        return entry;
-    }
-
-void insertLinkedMap1x1(HashMap *map, Entry *e) // tratamento de colisão por encadeamento
-    {
-        if(!map || !e)
-            return;
-        map->entries[hash1x1(e->value)] = e; // se for null, insere, se não for, tem que inserir de forma ordanda (implementar)
-    }
+  }
+  return map;
+}
+// 
+void insertLinkedMap1x1(HashMap *map, Entry *e) {
+  if(!map || !e) return;
+  if(map->lists[hash1x1(e->value)].length == 0) {
+    map->lists[hash1x1(e->value)].start = e;
+    map->lists[hash1x1(e->value)].length++;
+  } else { // inserção ordenada
+    
+  }
+}
