d69a8e93e8034d797a2db39ae02381dceeb7237a
Gustavo Cunha <gustavoparreira@hotmail.com>
2024-05-27 09:26:51 -0300

    tabelas hash

--- ED - Tabelas Hash/main.c
+++ ED - Tabelas Hash/main.c
@@ -25,10 +25,10 @@ int hash1x1(int x)
 
 int hash1xn(int x)
     {
-        
+        return x % 10;
     }
 
-    HashMap *criarHashMap(int tableSize) // representa a criação do modelo de lista encadeada
+HashMap *newHashMap(int tableSize) // representa a criação do modelo de lista encadeada
     {
         HashMap *map = malloc(sizeof(HashMap));
         if(map)
@@ -36,4 +36,23 @@ int hash1xn(int x)
                 map->entries = calloc(sizeof(Entry*), tableSize);
                 map->length = 0;
             }
+        return map;
+    }
+
+Entry *newEntry(int key, int value)
+    {
+        Entry *entry = malloc(sizeof(Entry));
+        if(entry)
+            {
+                entry->key = key;
+                entry->value = value;
+            }
+        return entry;
+    }
+
+void insertLinkedMap1x1(HashMap *map, Entry *e) // tratamento de colisão por encadeamento
+    {
+        if(!map || !e)
+            return;
+        map->entries[hash1x1(e->value)] = e; // se for null, insere, se não for, tem que inserir de forma ordanda (implementar)
     }
