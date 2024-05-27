0cb60381392496aca23668a1f42cabc66bb62c58
Gustavo Cunha <gustavoparreira@hotmail.com>
2024-05-27 09:14:58 -0300

    construção de código na aula

--- ED - Tabelas Hash/main.c
+++ ED - Tabelas Hash/main.c
@@ -9,13 +9,31 @@ struct entry //par chave-valor, representado como um nó, que tem ponteiro que a
         int key;
         int value;
         Entry *prox;
-    } 
+    };
 
 struct hash_map
     {
-        Entry *entry;
+        Entry **entries;
         int length;
-        void (*hashFunction)(int key) // ponteiro para funções
+        // void (*hashFunction)(int key) // ponteiro para funções
+    };
+
+int hash1x1(int x)
+    {
+        return x;
     }
 
-    
+int hash1xn(int x)
+    {
+        
+    }
+
+    HashMap *criarHashMap(int tableSize) // representa a criação do modelo de lista encadeada
+    {
+        HashMap *map = malloc(sizeof(HashMap));
+        if(map)
+            {
+                map->entries = calloc(sizeof(Entry*), tableSize);
+                map->length = 0;
+            }
+    }
